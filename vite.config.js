83a3e41e1afa918fb1fd55bc27d1111fdbf77851
Arely <onixuniverse@gmail.com>
2025-10-06 19:47:35 -0600

    Edit path resolve vite config con alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// 1. Importa los módulos necesarios de Node
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// 2. Recrea las variables __filename y __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Wordy/', 
+  base: '/Wordy/',
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // 3. path.resolve
+      '@profilepics': path.resolve(__dirname, './src/assets/ProfilePics'),
+    },
+  },
+});
